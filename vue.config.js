4fda016b52392522cb9ec05736e622cffd38ea37
YaCpotato <yasshisshy@gmail.com>
2021-03-27 23:52:57 +0900

    config edit

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-    build: {
-        indexPath: '../docs/index.html',
-        outputDir: '../docs/',
-        assetsDir: 'static',
-    }
+    indexPath: '../docs/index.html',
+    outputDir: '../docs/',
+    assetsDir: 'static',
   }

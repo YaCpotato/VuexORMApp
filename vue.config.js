0cbae483e598f153839f01b2f5864ee595051dc8
YaCpotato <yasshisshy@gmail.com>
2021-03-27 23:49:45 +0900

    config追加

---
+++ vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    build: {
+        index: path.resolve(__dirname, '../docs/index.html'),
+        assetsRoot: path.resolve(__dirname, '../docs'),
+        assetsSubDirectory: 'static',
+        assetsPublicPath: './'
+    }
+  }
